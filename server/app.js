99ea6095036841e7bccbce08cd2e898a34b31cc3
eclevens <ellaclevens@gmail.com>
2025-11-09 20:25:00 -0500

    domomaker b complete

--- server/app.js
+++ server/app.js
@@ -1,39 +1,47 @@
-const path = require("path");
-const express = require("express");
-const compression = require ("compression");
-const favicon = require("serve-favicon");
-const mongoose = require("mongoose");
-const expressHandlebars = require("express-handlebars");
-const helmet = require("helmet");
+const path = require('path');
+const express = require('express');
+const compression = require('compression');
+const favicon = require('serve-favicon');
+const mongoose = require('mongoose');
+const expressHandlebars = require('express-handlebars');
+const helmet = require('helmet');
+const session = require('express-session');
 
-const router = require("./router.js");
+const router = require('./router.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const dbURI = process.env.MONGODB_URI || "mongodb://localhost/DomoMaker";
+const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/DomoMaker';
 mongoose.connect(dbURI).catch((err) => {
-    if(err) {
-        console.log("Could not connect to database");
-        throw err;
-    }
+  if (err) {
+    console.log('Could not connect to database');
+    throw err;
+  }
 });
 
 const app = express();
 
 app.use(helmet());
-app.use("/assets", express.static(path.resolve(`${__dirname}/../hosted`)));
+app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.engine("handlebars", expressHandlebars.engine({ defaultLayout: "" }));
-app.set("view engine", "handlebars");
-app.set("views", `${__dirname}/../views`);
+app.use(session({
+  key: 'sessionid',
+  secret: 'Domo Arigato',
+  resave: false,
+  saveUninitialized: false,
+}));
+
+app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
+app.set('view engine', 'handlebars');
+app.set('views', `${__dirname}/../views`);
 
 router(app);
 
 app.listen(port, (err) => {
-    if(err) {throw err;}
-    console.log(`Listening on port ${port}`);
+  if (err) { throw err; }
+  console.log(`Listening on port ${port}`);
 });
